859619aeccadc01d48c4a0f46c94515e327f90bc
ruka-charo <ruka.and.little.charo1@gmail.com>
2021-02-21 23:49:40 +0900

    Search_day5

--- pdf_scraping.py
+++ pdf_scraping.py
@@ -1,1 +1,22 @@
-from 
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+
+input_path = 'image/03-yoshi.pdf'
+output_path = 'image/result.txt'
+
+rsrcmgr = PDFResourceManager()
+codec = 'utf-8'
+params = LAParams()
+
+
+with open(output_path, 'ab') as output:
+    device = TextConverter(rsrcmgr, output, codec=codec, laparams=params)
+    with open(input_path, 'rb') as input:
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.get_pages(input):
+            interpreter.process_page(page)
+
+    device.close()
